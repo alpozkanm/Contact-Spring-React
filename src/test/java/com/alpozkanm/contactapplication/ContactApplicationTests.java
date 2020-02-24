1243d22e64dd581a790b7ab891e643e6fcab964d
Alp Ozkan <26024533@A00102586>
2020-02-24 13:41:32 +0300

    First commit

---
+++ src/test/java/com/alpozkanm/contactapplication/ContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alpozkanm.contactapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
